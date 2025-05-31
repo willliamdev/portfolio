797f16e0c2e260c822fb1efd98de67d961bae3a0
William Almeida <willliam.dev@gmail.com>
2025-05-31 18:16:54 -0300

    bug fix: duplicate home load, create a new `render` function

--- script/script.js
+++ script/script.js
@@ -1,29 +1,33 @@
+
+const main = document.getElementById('conteudo-principal');
+
+function render(pagina) {
+    fetch(pagina)
+        .then(response => {
+            if (!response.ok) throw new Error('Erro ao carregar ' + pagina);
+            return response.text();
+        })
+        .then(html => {
+            main.innerHTML = html;
+        })
+        .catch(error => {
+            main.innerHTML = `<p style="color:red;">Erro ao carregar conteúdo: ${error.message}</p>`;
+        });
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
+
+    const home = "pages/home.html"
+    render(home);
+
     const links = document.querySelectorAll('nav a[data-pagina]');
-    const main = document.getElementById('conteudo-principal');
 
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
             const pagina = this.getAttribute('data-pagina');
-
-            if (pagina === 'index.html') {
-                // Restaurar conteúdo inicial do main
-                main.innerHTML = '<h2>Bem vindo!</h2><p>Conteúdo principal</p>';
-                return;
-            }
-
-            fetch(pagina)
-                .then(response => {
-                    if (!response.ok) throw new Error('Erro ao carregar ' + pagina);
-                    return response.text();
-                })
-                .then(html => {
-                    main.innerHTML = html;
-                })
-                .catch(error => {
-                    main.innerHTML = `<p style="color:red;">Erro ao carregar conteúdo: ${error.message}</p>`;
-                });
+            render(pagina)
         });
     });
 });
